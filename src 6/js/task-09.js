0104d385a3edc1798e6b04d7dcbe976ad6d437e0
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2022-12-24 11:26:27 +0200

    update js

--- src 6/js/task-09.js
+++ src 6/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorBtn = document.querySelector("button.change-color");
+const body = document.querySelector('body');
+const span = document.querySelector('span.color');
+
+colorBtn.addEventListener('click', () =>{
+  let color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+
+})
+
