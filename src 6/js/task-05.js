110b5a7d7a4124174bde97952da049907518c101
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2022-12-24 11:43:22 +0200

    update js

--- src 6/js/task-05.js
+++ src 6/js/task-05.js
@@ -1,6 +1,7 @@
 let nameInput = document.getElementById("name-input");
 let nameOutput = document.getElementById("name-output");
 
+
 nameInput.oninput = function () {
     if (nameInput.value === '') {
        nameOutput.innerHTML = 'Anonymous';
